351a8071e294470cc856be17f37c89e674e6df16
dusandobranic <dusan.dobranic@gmail.com>
2021-12-04 15:55:40 +0100

    Added js for input elements and operators

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const operations = document.querySelector(".operations");
 const divs = operations.querySelectorAll("div");
 const numbers = document.querySelectorAll(".num");
+const operator = document.querySelectorAll(".operator");
 
 const calculator = {
   displayValue: "0",
@@ -18,24 +19,19 @@ const keys = Array.from(document.querySelectorAll(".item"));
 keys.forEach((key) => {
   key.addEventListener("click", (event) => {
     const { target } = event;
-    console.log(target)
-
-    if (!target.classList.contains("item")) {
-      return;
-    }
 
     if (target.classList.contains("operator")) {
-      console.log("operator", target.innerText);
+      operatorClicked(target.innerText);
       return;
     }
 
     if (target.classList.contains("decimal")) {
-      console.log("decimal", target.innerText);
+      addDecimal(".");
       return;
     }
 
     if (target.classList.contains("all-clear")) {
-      console.log("clear", target.innerText);
+      allClear();
       return;
     }
     if (target.classList.contains("equals")) {
@@ -48,17 +44,63 @@ keys.forEach((key) => {
   });
 });
 
-function updateDisplay() {
-  const display = document.querySelector("#display");
-  display.value = calculator.displayValue;
+function addDecimal(dot) {
+  if (calculator.displayValue.includes('.')) {
+    return;
+  } else {
+    calculator.displayValue = calculator.displayValue + dot;
+  }
+  updateDisplay();
+}
 
-  console.log(calculator.displayValue);
-  console.log(display);
+function updateDisplay() {
+  const _display = document.querySelector("#display");
+  _display.value = calculator.displayValue;
 }
 
 function inputDigit(digit) {
   const { displayValue } = calculator;
   calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+}
+
+function clearDisplay() {
+  waitingForSecondOperand = true;
+  calculator.displayValue = "";
+  updateDisplay();
+}
+
+function allClear() {
+  calculator.displayValue = "0";
+  updateDisplay();
+}
+
+function operatorClicked(operator) {
+  actions = operator;
+  switch (actions) {
+    case "+":
+      calculator.operator = "+"
+      calculator.firstOperand = calculator.displayValue;
+      clearDisplay();
+      break;
+    case "-":
+      calculator.operator = "-";
+      firstOperand = calculator.displayValue;
+      clearDisplay();
+      break;
+    case "*":
+      calculator.operator = "*";
+      calculator.firstOperand = calculator.displayValue;
+      clearDisplay();
+      break;
+    case "/":
+      calculator.operator = "/";
+      calculator.firstOperand = calculator.displayValue;
+      clearDisplay();
+      break;
+    default:
+      alert("Nije unet ispravan znak");
+      break;
+  }
 }
 
 updateDisplay();
